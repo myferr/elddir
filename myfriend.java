8dcae885d3c31a120d691173766c8574fc785641
MyferIsADev <149029289+myferr@users.noreply.github.com>
2024-03-06 12:07:27 -0600

    Create myfriend.java

---
+++ myfriend.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("http://");
+        System.out.println(url());
+        System.out.println("/u/--------------------------");
+        System.out.println("dm me when ");
+        dontyou();
+    }
+
+    public static String url() {
+        return "www.guilded.gg";
+    }
+
+    public static void dontyou() {
+        System.out.println("you get it");
+    }
+}
