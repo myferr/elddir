9c5836eeb2bdbdfe23f0c1447d5ea1bc8e21eb6f
MyferIsADev <149029289+myferr@users.noreply.github.com>
2024-03-06 11:50:24 -0600

    Create secret.js

---
+++ huh/secret.js
@@ -0,0 +1,18 @@
+function msESAge() {
+    return "LRU"
+  }
+function KDI() {
+    return "yfitops"
+}
+  
+  function secret2() {
+    console.log(`its my ${msESAge()}`)
+  }
+  function OWT() {
+    console.log(`secret`)
+  }
+  function eno_terces() {
+    console.log(`its my ${(KDI())}`)
+  }
+  secret2()
+  eno_terces()
